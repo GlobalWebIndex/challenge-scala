9834f1a2745902748928ba08065e39adf1d7419b
migmit <migmit@gmail.com>
2023-01-19 03:54:07 +0100

    Streaming task's state

--- app/controllers/CsvToJsonController.scala
+++ app/controllers/CsvToJsonController.scala
@@ -1,22 +1,31 @@
 package controllers
 
+import akka.NotUsed
+import akka.stream.scaladsl.Source
 import conversion.ConversionService
 import models.TaskCurrentState
+import models.TaskInfo
+import play.api.Configuration
 import play.api.libs.json.Json
 import play.api.libs.json.Writes.keyMapWrites
 import play.api.mvc.AbstractController
 import play.api.mvc.Action
 import play.api.mvc.ControllerComponents
+import play.api.mvc.RequestHeader
 
 import java.io.File
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
 
 class CsvToJsonController(
+    config: Configuration,
     controllerComponents: ControllerComponents,
     conversionService: ConversionService
 )(implicit
     ec: ExecutionContext
 ) extends AbstractController(controllerComponents) {
+  val pollingPeriod =
+    Duration(config.get[Long]("csvToJson.pollingPeriodMillis"), "ms")
 
   def createTask: Action[String] = Action.async(parse.tolerantText) {
     implicit request =>
@@ -27,15 +36,16 @@ class CsvToJsonController(
       Ok(Json.toJson(tasks.map(taskInfoToDetails).toMap))
     )
   }
-  def taskDetails(taskId: String): Action[Unit] = Action.async(parse.empty) {
-    implicit request =>
-      conversionService
-        .getTask(taskId)
-        .map(_ match {
-          case None          => NotFound("No such task")
-          case Some(details) => Ok(Json.toJson(taskInfoToDetails(details)._2))
-        })
-  }
+  def taskDetails(taskId: String): Action[Unit] =
+    Action.async(parse.empty) { // TOFIX: update every two seconds
+      implicit request =>
+        conversionService
+          .getTask(taskId)
+          .map(_ match {
+            case None       => NotFound("No such task")
+            case Some(info) => Ok.chunked(taskDetailsStream(info), None)
+          })
+    }
   def cancelTask(taskId: String): Action[Unit] = Action.async(parse.empty) {
     implicit request =>
       conversionService
@@ -62,4 +72,23 @@ class CsvToJsonController(
             }
         })
   }
+
+  private def taskDetailsStream(
+      taskInfo: TaskInfo
+  )(implicit request: RequestHeader) =
+    Source
+      .tick(pollingPeriod, pollingPeriod, NotUsed)
+      .mapAsync(1)(_ => conversionService.getTask(taskInfo.taskId))
+      .collect({ case Some(j) => j })
+      .prepend(Source.single(taskInfo))
+      .takeWhile(
+        _.state match {
+          case TaskCurrentState.Done(_, _) => false
+          case _                           => true
+        },
+        inclusive = true
+      )
+      .map(info => Json.stringify(Json.toJson(taskInfoToDetails(info)._2)))
+      .intersperse("\n")
+
 }
